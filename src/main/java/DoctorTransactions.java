dc6f4e653399e9f4e530a96cc49da263f5c4aaab
Enes Faruk Baltacı <efarukbaltaci@gmail.com>
2022-05-19 22:04:00 +0200

    v.2

--- src/main/java/DoctorTransactions.java
+++ src/main/java/DoctorTransactions.java
@@ -2,7 +2,7 @@ import java.sql.*;
 import java.util.Scanner;
 
 
-public class DoctorTransactions extends Connector implements Transactions  {
+public class DoctorTransactions extends Connector implements Transactions {
 
 
     static Scanner scan = new Scanner(System.in);
@@ -17,37 +17,37 @@ public class DoctorTransactions extends Connector implements Transactions  {
         Doctor doctor = new Doctor();
         System.out.print("Doktor ismini giriniz : ");
         doctor.setAd(scan.nextLine());
-       // st.executeUpdate("insert into"+" Doktorlar("+ "ad)"  +" values ("+doctor.getAd()+")");
+        // st.executeUpdate("insert into"+" Doktorlar("+ "ad)"  +" values ("+doctor.getAd()+")");
         System.out.print("Doktor soyismini giriniz : ");
         doctor.setSoyad(scan.nextLine());
-       // st.executeUpdate("insert into"+" Doktorlar("+ "soyad)"  +" values ("+doctor.getSoyad()+")");
+        // st.executeUpdate("insert into"+" Doktorlar("+ "soyad)"  +" values ("+doctor.getSoyad()+")");
         System.out.print("Doktor dogum tarihini giriniz : ");
         doctor.setDogumT(scan.nextLine());
-      //  st.executeUpdate("insert into"+" Doktorlar("+ "dT)"  +" values ("+doctor.getDogumT()+")");
+        //  st.executeUpdate("insert into"+" Doktorlar("+ "dT)"  +" values ("+doctor.getDogumT()+")");
         System.out.print("Doktor bransini giriniz : ");
         doctor.setBrans(scan.nextLine());
-       // st.executeUpdate("insert into"+" Doktorlar("+ "brans)"  +" values ("+doctor.getBrans()+")");
+        // st.executeUpdate("insert into"+" Doktorlar("+ "brans)"  +" values ("+doctor.getBrans()+")");
         System.out.print("Doktor maasini giriniz : ");
         doctor.setMaas(scan.nextInt());
-      //  st.executeUpdate("insert into"+" Doktorlar("+ "maas)"  +" values ("+doctor.getMaas()+")");
-        st.executeUpdate("insert into Doktorlar values (" + doctor+")");
-      //  System.out.println(doctor);
+        //  st.executeUpdate("insert into"+" Doktorlar("+ "maas)"  +" values ("+doctor.getMaas()+")");
+        st.executeUpdate("insert into Doktorlar values (" + doctor + ")");
+        //  System.out.println(doctor);
 
-    //    insert into Doktorlar values (doctor);
+        //    insert into Doktorlar values (doctor);
 
     }
- // CRUD -- Create Read Update Delete
+
+    // CRUD -- Create Read Update Delete
     @Override
     public void read() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hastahane?serverTimezone=UTC", "root", "1234");
         Statement st = con.createStatement();
-        ResultSet veri =  st.executeQuery("select * from Doktorlar");
-        System.out.printf("%-6s %-15.15s %-8s %-8s %-15s %-8s\n","id", "Doktor Isim","Doktor Soyisim","Dogum Tarihi","Brans","Maas");
+        ResultSet veri = st.executeQuery("select * from Doktorlar");
+        System.out.printf("%-6s %-15.15s %-8s %-8s %-15s %-8s\n", "id", "Doktor Isim", "Doktor Soyisim", "Dogum Tarihi", "Brans", "Maas");
         System.out.println("====== ============== ========  ======= ============== ======== ");
-
-        while(veri.next()){
-            System.out.printf("%-6d %-15.15s %-8s %-8s %-15s %-8d\n", veri.getInt(1), veri.getString(2), veri.getString(3), veri.getString(4),veri.getString(5),veri.getInt(6));
+        while (veri.next()) {
+            System.out.printf("%-6d %-15.15s %-8s %-8s %-15s %-8d\n", veri.getInt(1), veri.getString(2), veri.getString(3), veri.getString(4), veri.getString(5), veri.getInt(6));
         }
 
     }
@@ -58,21 +58,31 @@ public class DoctorTransactions extends Connector implements Transactions  {
         // TODO Kullanicidan hangi veriyi guncellemek istediginin numarasini al index ata switch/case calis
         System.out.print("Lutfen degisiklik yapmak istediginiz doktorun ID'sini giriniz : ");
         int doktorID = scan.nextInt();
-        System.out.println("1-Doktor Ismi\n2-Doktor Soyisim\n3-Dogum Tarihi\n4-Brans\n5-Maas" +
+        System.out.print("1-Doktor Ismi\n2-Doktor Soyisim\n3-Dogum Tarihi\n4-Brans\n5-Maas" +
                 "\nLutfen menuden degisiklik yapmak istediginiz bilgiyi seciniz : ");
         int degisiklikIndex = scan.nextInt();
         scan.nextLine();
-        switch (degisiklikIndex){
-            case 1 : doktorIsimDegistir(doktorID);
-            break;
-            case 2 : doktorSoyisimDegistir(doktorID);
-            break;
-            case 3 : doktorDTarihiDegistir(doktorID);
-            break;
-            case 4 : doktorBransDegistir(doktorID);
-            break;
-            case 5 : doktorMaasDegistir(doktorID);
-            break;
+        switch (degisiklikIndex) {
+            case 1:
+                String ad = "ad";
+                doktorIsimDegistir(ad, doktorID);
+                break;
+            case 2:
+                String soyad = "soyad";
+                doktorIsimDegistir(soyad, doktorID);
+                break;
+            case 3:
+                String dT = "dT";
+                doktorIsimDegistir(dT, doktorID);
+                break;
+            case 4:
+                String brans = "brans";
+                doktorIsimDegistir(brans, doktorID);
+                break;
+            case 5:
+                doktorMaasDegistir(doktorID);
+                break;
+
             default:
                 System.out.println("Salak salak gonusma be");
         }
@@ -82,59 +92,59 @@ public class DoctorTransactions extends Connector implements Transactions  {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hastahane?serverTimezone=UTC", "root", "1234");
         Statement st = con.createStatement();
-        ResultSet doktorVeri = st.executeQuery("select * from Doktorlar where id="+doktorID);
+        ResultSet doktorVeri = st.executeQuery("select * from Doktorlar where id=" + doktorID);
         // System.out.println("Isim bilgisini degistirmek istediginiz doktor : " + doktorVeri.getString(1));
-        System.out.println("Doktorun guncel maasini giriniz : ");
+        System.out.print("Doktorun guncel maasini giriniz : ");
         int yeniMaas = scan.nextInt();
-        st.executeUpdate("update Doktorlar set maas='"+yeniMaas +"' where id="+doktorID);
+        st.executeUpdate("update Doktorlar set maas='" + yeniMaas + "' where id=" + doktorID);
         read();
     }
 
-    private void doktorBransDegistir(int doktorID) throws ClassNotFoundException, SQLException {
+//    private void doktorBransDegistir(int doktorID) throws ClassNotFoundException, SQLException {
+//        Class.forName("com.mysql.cj.jdbc.Driver");
+//        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hastahane?serverTimezone=UTC", "root", "1234");
+//        Statement st = con.createStatement();
+//        ResultSet doktorVeri = st.executeQuery("select * from Doktorlar where id=" + doktorID);
+//        // System.out.println("Isim bilgisini degistirmek istediginiz doktor : " + doktorVeri.getString(1));
+//        System.out.println("Doktorun yeni bransini giriniz : ");
+//        String yeniBrans = scan.nextLine();
+//        st.executeUpdate("update Doktorlar set brans='" + yeniBrans + "' where id=" + doktorID);
+//        read();
+//    }
+
+//    private void doktorDTarihiDegistir(int doktorID) throws ClassNotFoundException, SQLException {
+//        Class.forName("com.mysql.cj.jdbc.Driver");
+//        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hastahane?serverTimezone=UTC", "root", "1234");
+//        Statement st = con.createStatement();
+//        ResultSet doktorVeri = st.executeQuery("select * from Doktorlar where id=" + doktorID);
+//        // System.out.println("Isim bilgisini degistirmek istediginiz doktor : " + doktorVeri.getString(1));
+//        System.out.println("Yeni doktor dogum tarihini giriniz : ");
+//        String yeniDTarihi = scan.nextLine();
+//        st.executeUpdate("update Doktorlar set dT='" + yeniDTarihi + "' where id=" + doktorID);
+//        read();
+//    }
+
+//    private void doktorSoyisimDegistir(int doktorID) throws ClassNotFoundException, SQLException {
+//        Class.forName("com.mysql.cj.jdbc.Driver");
+//        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hastahane?serverTimezone=UTC", "root", "1234");
+//        Statement st = con.createStatement();
+//        ResultSet doktorVeri = st.executeQuery("select * from Doktorlar where id=" + doktorID);
+//        // System.out.println("Isim bilgisini degistirmek istediginiz doktor : " + doktorVeri.getString(1));
+//        System.out.println("Yeni doktor soyismini giriniz : ");
+//        String yeniSoyIsim = scan.nextLine();
+//        st.executeUpdate("update Doktorlar set soyad='" + yeniSoyIsim + "' where id=" + doktorID);
+//        read();
+//    }
+
+    private void doktorIsimDegistir(String column, int doktorID) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hastahane?serverTimezone=UTC", "root", "1234");
         Statement st = con.createStatement();
-        ResultSet doktorVeri = st.executeQuery("select * from Doktorlar where id="+doktorID);
+        // ResultSet doktorVeri = st.executeQuery("select * from Doktorlar where id="+doktorID);
         // System.out.println("Isim bilgisini degistirmek istediginiz doktor : " + doktorVeri.getString(1));
-        System.out.println("Doktorun yeni bransini giriniz : ");
-        String yeniBrans = scan.nextLine();
-        st.executeUpdate("update Doktorlar set brans='"+yeniBrans +"' where id="+doktorID);
-        read();
-    }
-
-    private void doktorDTarihiDegistir(int doktorID) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hastahane?serverTimezone=UTC", "root", "1234");
-        Statement st = con.createStatement();
-        ResultSet doktorVeri = st.executeQuery("select * from Doktorlar where id="+doktorID);
-        // System.out.println("Isim bilgisini degistirmek istediginiz doktor : " + doktorVeri.getString(1));
-        System.out.println("Yeni doktor dogum tarihini giriniz : ");
-        String yeniDTarihi = scan.nextLine();
-        st.executeUpdate("update Doktorlar set dT='"+yeniDTarihi +"' where id="+doktorID);
-        read();
-    }
-
-    private void doktorSoyisimDegistir(int doktorID) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hastahane?serverTimezone=UTC", "root", "1234");
-        Statement st = con.createStatement();
-        ResultSet doktorVeri = st.executeQuery("select * from Doktorlar where id="+doktorID);
-        // System.out.println("Isim bilgisini degistirmek istediginiz doktor : " + doktorVeri.getString(1));
-        System.out.println("Yeni doktor soyismini giriniz : ");
-        String yeniSoyIsim = scan.nextLine();
-        st.executeUpdate("update Doktorlar set soyad='"+yeniSoyIsim +"' where id="+doktorID);
-        read();
-    }
-
-    private void doktorIsimDegistir(int doktorID) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hastahane?serverTimezone=UTC", "root", "1234");
-        Statement st = con.createStatement();
-       // ResultSet doktorVeri = st.executeQuery("select * from Doktorlar where id="+doktorID);
-       // System.out.println("Isim bilgisini degistirmek istediginiz doktor : " + doktorVeri.getString(1));
-        System.out.println("Yeni doktor ismini giriniz : ");
+        System.out.print("Yeni doktor " + column + " giriniz : ");
         String yeniIsim = scan.nextLine();
-        st.executeUpdate("update Doktorlar set ad='"+yeniIsim +"' where id="+doktorID);
+        st.executeUpdate("update Doktorlar set " + column + "='" + yeniIsim + "' where id=" + doktorID);
         read();
     }
 
@@ -146,7 +156,7 @@ public class DoctorTransactions extends Connector implements Transactions  {
         read();
         System.out.print("Silmek istediginiz doktorun ID sini giriniz : ");
         int input = scan.nextInt();
-        st.executeUpdate("delete from Doktorlar where id="+input);
+        st.executeUpdate("delete from Doktorlar where id=" + input);
 
     }
 }
